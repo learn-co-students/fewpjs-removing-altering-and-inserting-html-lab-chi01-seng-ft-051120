5dcf1657654f4fd87497bcf6e0b15085a5a647e1
Sam Escobar <sescobar8714@gmail.com>
2020-06-23 10:44:34 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+"Make sure you remove the <main> with id 'main'"
+
+main.remove()
+
+"Make sure you create an <h1> with id 'victory'"
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.className = "victory";
