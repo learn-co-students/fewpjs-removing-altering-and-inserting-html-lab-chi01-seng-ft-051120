a0f61cdab7fa34c7e739eae830b2fab4fdfd9fc2
Claudiu Sarb <sarb.claudiu391@gmail.com>
2020-06-24 13:59:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+main.remove();
+
+let victory = document.createElement('h1');
+victory.setAttribute("id", "victory");
+document.body.appendChild(victory);
+
+let newHeader = document.querySelector("h1#victory");
+newHeader.innerHTML = 'Claudiu is the champion'
